4ef8e20157205f13a729c19ea4499f67900553e2
jaggs07 <jagaran@fusemachines.com>
2021-03-03 00:31:01 +0545

    Implement socket along with polygon

---
+++ src/index.js
@@ -0,0 +1,30 @@
+var app = require('express')()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
+var { PORT } = require('../config')
+const Polygon = require('./polygon')
+
+
+io.on('connection', function (socket) {
+  const client = new Polygon({ apiKey: "rCPMgpXH80NfZSGMDjGhmPKloJHQlxbg" })
+  client.subscribe(['XT.*'])
+
+  client.on('XT', (trade) => {
+    socket.emit('trade_data', trade )
+  })
+})
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.use('/*', (req, res, next) => {
+  res.send({
+    status: 404,
+    message: 'No route defined'
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
